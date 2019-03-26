942e0fcea690058eb6601b30d9843d733e668238
Eryk Mroczko <e.mroczko@stud.elka.pw.edu.pl>
2019-03-26 14:09:11 +0100

    6th commit

---
+++ PROEprojekt1/PROEprojekt1/MainObjectShop.cpp
@@ -0,0 +1,9 @@
+//
+//  MainObjectShop.cpp
+//  PROEprojekt1
+//
+//  Created by Eryk Mroczko on 21/03/2019.
+//  Copyright © 2019 Eryk Mroczko. All rights reserved.
+//
+
+#include "MainObjectShop.hpp"
---
+++ PROEprojekt1/PROEprojekt1/MainObjectShop.hpp
@@ -0,0 +1,14 @@
+//
+//  MainObjectShop.hpp
+//  PROEprojekt1
+//
+//  Created by Eryk Mroczko on 21/03/2019.
+//  Copyright © 2019 Eryk Mroczko. All rights reserved.
+//
+
+#ifndef MainObjectShop_hpp
+#define MainObjectShop_hpp
+
+#include <stdio.h>
+
+#endif /* MainObjectShop_hpp */
